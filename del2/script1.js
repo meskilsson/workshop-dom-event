786edf5c21621b04ff5c8b475740478fcb8952ea
Mattias Eskilsson <mattiaseskilsson@hotmail.se>
2025-10-16 12:21:17 +0200

    del2 övning 1

--- del2/script1.js
+++ del2/script1.js
@@ -1,7 +1,29 @@
 
-const btnColor=document.getElementById("btnColor");
-btnColor.onclick=function() {
+const btnColor = document.getElementById("btnColor");
+const btnAdd = document.getElementById('btnAdd');
+const ptag = document.querySelector('p');
+const btnClear = document.getElementById('btnClear');
+
+
+btnColor.onclick = function () {
     btnColor.style.backgroundColor = "black";
-
-
 }
+
+
+
+btnAdd.onclick = function () {
+    ptag.textContent += "blablabllabla";
+}
+
+
+// btnClear.onclick = function () {
+//     ptag.textContent = "";
+// }
+
+const clear = function () {
+    btnClear.addEventListener('click', () => {
+        ptag.textContent = "";
+    });
+}
+
+clear();
